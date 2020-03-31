d22c02ec88cc8a5eea3cae50d07832c9b7b3f45c
Srishti Gajare <srishtigajare@srishtis-mbp.lan>
2020-03-31 18:56:34 -0500

    all new changes

--- Online_retail_management/settings.py
+++ Online_retail_management/settings.py
@@ -84,6 +84,15 @@ DATABASES = {
     }
 }
 
+# #DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'sp',
+#         'PASSWORD': 'test',
+#         'USER': 'root',
+#     }
+# }
+
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
